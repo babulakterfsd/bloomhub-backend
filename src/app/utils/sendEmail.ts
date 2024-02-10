5e38678d515971de91c86b9a8a736aeda1caa061
Babul Akter <babulakterfsd@gmail.com>
2024-02-10 15:37:41 +0600

    nodemailer setup done

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,6 +1,8 @@
 import nodemailer from 'nodemailer';
 import config from '../config';
 
+/* ei nodemailer er setup local e khub shundor kaj kore, kntu vercel evabe mail sending support kore na ar, tai amar backend thikthak kaj korleo, shopkeeper er mail e mail jabe na. tao ei setup ta rakhlam karon kono paid hosting hole tkhn ei setup e aramche kaj korbe in sha allah.  */
+
 export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com.',
